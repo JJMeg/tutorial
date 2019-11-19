4c2b12638a6a12797195c45f7ecea6db7fd04337
hexiaoying10 <hexiaoying10@git.jd.com>
2019-11-19 16:00:25 +0800

    feat: init project and add word count

---
+++ src/main/scala/com/jjmeg/wordcount.scala
@@ -0,0 +1,24 @@
+package com.jjmeg
+
+import org.apache.flink.api.scala._
+
+// 批处理wordcount程序
+object wordcount {
+  def main(args: Array[String]): Unit = {
+    //    创建执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    //    从文件中读取数据
+    val inputPath = "D:\\IntelliJ IDEA 2019.2.3\\tutorial\\src\\main\\resources\\hello.txt"
+    val inputDataSet = env.readTextFile(inputPath)
+
+    //    做count处理
+    //    切分数据，得到word，按word做分组聚合
+    val wordCountDataSet = inputDataSet.flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    wordCountDataSet.print()
+  }
+}
