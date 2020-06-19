b80300bf28d638c11676e220626368458b9a6594
jjmeg <flightlessbird-go@hotmail.com>
2020-06-19 09:16:08 +0800

    feat: add status

--- src/main/scala/com/jjmeg/ProcessFunction.scala
+++ src/main/scala/com/jjmeg/ProcessFunction.scala
@@ -1,5 +1,6 @@
 package com.jjmeg
 
+import akka.japi.Option.Some
 import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.api.common.state
 import org.apache.flink.api.common.state.ValueStateDescriptor
@@ -7,22 +8,52 @@ import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
 import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
-import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.time.Time
 
 object ProcessFunction {
-  //    创建批处理执行环境
-  val env = ExecutionEnvironment.getExecutionEnvironment
+  def main(args: Array[String]): Unit = {
+    //    创建批处理执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-  val inputPath = "D:\\IntelliJ IDEA 2019.2.3\\tutorial\\src\\main\\resources\\hello.txt"
-  val streamFromFile = env.readTextFile(inputPath)
+    val stream: DataStream[String] = env.socketTextStream("localhost", 7777)
 
-  val stream = streamFromFile
-    .map(
-      data => {
-        val dataArray = data.split(",")
-        SensorReading(dataArray(0).trim, dataArray(1).trim.toLong, dataArray(2).trim.toDouble)
-      }
-    )
+    //    逐一取每条数据处理，打散之后进行word count
+    val dataStream = stream.map(data => {
+      val dataArray = data.split(",")
+      SensorReading(dataArray(0).trim, dataArray(1).trim.toLong, dataArray(2).trim.toDouble)
+    })
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
+        override def extractTimestamp(t: SensorReading): Long = t.timestamp * 1000
+      })
+
+    val processStream = dataStream.keyBy(_.id)
+      .process(new TempChangeAlert(10.0))
+
+    val processStream2 = dataStream.keyBy(_.id)
+      .flatMap(new FlatMapTempChangeAlert(10.0))
+
+    //    R：输出类型， S：状态
+    val processStream3 = dataStream.keyBy(_.id)
+      .flatMapWithState[(String, Double, Double), Double] {
+      //      如果没有状态，也就是没数据来过，那就将当前温度值存入状态
+      case (input: SensorReading, None) => (List.empty, Some(input.temperature))
+      //        如果有状态，应该比较上次的状态
+      case (input: SensorReading, lastTemp) =>
+        val diff = (input.temperature - lastTemp.get).abs
+        if (diff > 10) {
+          (List((input.id, lastTemp.get, input.temperature)), Some(input.temperature))
+        } else {
+          (List.empty, Some(input.temperature))
+        }
+
+    }
+
+    env.execute()
+  }
+
+
 }
 
 
